f0623266930b6d4f06049f0295718b66e272edd0
SZ7M8ci7 <83046414+SZ7M8ci7@users.noreply.github.com>
2024-03-09 19:11:36 +0900

    エラーハンドリング修正

--- main.py
+++ main.py
@@ -8,32 +8,33 @@ PRIVATE_KEY = os.getenv('PRIVATE_KEY').replace('\\n', '\n')
 CLIENT_EMAIL = os.getenv('CLIENT_EMAIL').replace('\\n', '\n')
 CLIENT_ID = os.getenv('CLIENT_ID').replace('\\n', '\n')
 CLIENT_X509_CERT_URL = os.getenv('CLIENT_X509_CERT_URL').replace('\\n', '\n')
+try:
+    # 認証情報の読み込み
+    scope = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    client_credentials = {
+        "type": "service_account",
+        "project_id": PROJECT_ID,
+        "private_key_id": PRIVATE_KEY_ID,
+        "private_key": PRIVATE_KEY,
+        "client_email": CLIENT_EMAIL,
+        "client_id": CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": CLIENT_X509_CERT_URL
+    }
 
-# 認証情報の読み込み
-scope = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-client_credentials = {
-  "type": "service_account",
-  "project_id": PROJECT_ID,
-  "private_key_id": PRIVATE_KEY_ID,
-  "private_key": PRIVATE_KEY,
-  "client_email": CLIENT_EMAIL,
-  "client_id": CLIENT_ID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": CLIENT_X509_CERT_URL
-}
+    creds = ServiceAccountCredentials._from_parsed_json_keyfile(client_credentials, scope, None, None)
+    client = gspread.authorize(creds)
+    # スプレッドシートのURLとシート名を指定する
+    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1p5CQHE-mHimEwpgAfU5BX9Fd4WMlfQAicTsq3icrYN0/edit?usp=sharing'
+    sheet_name = '家具データ'
 
-creds = ServiceAccountCredentials._from_parsed_json_keyfile(client_credentials, scope, None, None)
-client = gspread.authorize(creds)
-# スプレッドシートのURLとシート名を指定する
-spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1p5CQHE-mHimEwpgAfU5BX9Fd4WMlfQAicTsq3icrYN0/edit?usp=sharing'
-sheet_name = '家具データ'
-
-# スプレッドシートのデータを取得する
-worksheet = client.open_by_url(spreadsheet_url).worksheet(sheet_name)
-rows = worksheet.get_all_values()
-
+    # スプレッドシートのデータを取得する
+    worksheet = client.open_by_url(spreadsheet_url).worksheet(sheet_name)
+    rows = worksheet.get_all_values()
+except:
+    exit
 # 取得したデータを処理する
 with open('data.csv', mode='w', encoding='UTF-8') as file:
     count = 0
